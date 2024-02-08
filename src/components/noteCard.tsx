e0cd32d7d2f633afc3a8637845b29467c1c19a09
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-02-07 21:52:09 -0300

    fromatacao do titulo da nota

--- src/components/noteCard.tsx
+++ src/components/noteCard.tsx
@@ -16,7 +16,7 @@ export function NoteCard(props: Readonly<NoteCardProps>){
           <Dialog.Trigger className="rounded-md text-left bg-slate-800 p-5 flex flex-col gap-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 outline-none">
             
             <span className="text-sm font-medium text-slate-300">
-              {props.note.date.toISOString()}
+              {formatDistanceToNow(props.note.date, {locale: ptBR, addSuffix: true})}
             </span>
             <p className="text-sm leading-6 text-slate-400">
               {props.note.content}
